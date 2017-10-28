4456538506c24da49f8c5b8dbfa112732488b6e5
Amit Thapa <amit@okay.com>
2017-10-28 20:46:11 +0530

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	courses = [
-   {id: 1, name: 'Python'} ,
-   {id: 2, name: 'NodeJs'} ,
-   {id: 3, name: 'Angular 4'} ,
-   {id: 4, name: 'MongoDB'} ,
-   {id: 5, name: 'Express JS'} ,
-  ];
+	courses;
 
+  loadCourses() {
+    this.courses =  [
+      {id: 1, name: 'Python'} ,
+      {id: 2, name: 'NodeJs'} ,
+      {id: 3, name: 'Angular 4'} ,
+      {id: 4, name: 'MongoDB'} ,
+      {id: 5, name: 'Express JS'} ,
+     ];
+  }
  
+  trackByCoursId(index, course) {
+    return course? course.id : undefined;
+  }
 }
